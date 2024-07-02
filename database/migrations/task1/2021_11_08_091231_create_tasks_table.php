a853e0e03f5793822b2edee20c6b224613cf6a25
Imtyaz Ahmed <54882826+imtyaz-17@users.noreply.github.com>
2024-07-02 15:19:54 +0600

    T1 2021_11_08_091231_create_tasks_table.php

--- database/migrations/task1/2021_11_08_091231_create_tasks_table.php
+++ database/migrations/task1/2021_11_08_091231_create_tasks_table.php
@@ -16,9 +16,10 @@ class CreateTasksTable extends Migration
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
             $table->bigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
